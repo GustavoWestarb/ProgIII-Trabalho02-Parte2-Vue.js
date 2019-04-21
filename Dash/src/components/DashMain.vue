9c004a017223eee4f15679a07dbffe75fb8760ad
Gustavo Westarb <gustavowestarb45@gmail.com>
2019-04-21 08:47:56 -0300

    Implementações do Dashboard

--- Dash/src/components/DashMain.vue
+++ Dash/src/components/DashMain.vue
@@ -1,63 +1,108 @@
 <template>
-    <div class="page-container md-layout-row">
-    <md-app>
-      <md-app-toolbar class="md-primary header">
-        <span class="md-title">Dash</span>
-      </md-app-toolbar>
+  <div class="page-container md-layout-row">
+      <md-app>
+        <md-app-toolbar class="md-primary header">
+          <span class="md-title">Dash</span>
+        </md-app-toolbar>
 
-      <md-app-drawer md-permanent="full">
-        <md-toolbar class="md-transparent logo-menu" md-elevation="0">
-          LOGO
-        </md-toolbar>
+        <md-app-drawer md-permanent="full">
+          <md-toolbar class="md-transparent logo-menu" md-elevation="0">
+            LOGO
+          </md-toolbar>
 
-        <div class="full-control">
-        <div class="list">
-        <md-list :md-expand-single="expandSingle">
-            <md-list-item md-expand :md-expanded.sync="expandNews">
-            <md-icon>whatshot</md-icon>
-            <span class="md-list-item-text">News</span>
+          <div class="sidebar-wrapper">
+            <div class="list">
+              <md-list :md-expand-single="expandSingle">
+                  <md-list-item>
+                    <md-icon>card_travel</md-icon>
+                    <span class="md-list-item-text">Disciplinas</span>
+                  </md-list-item>
 
-            <md-list slot="md-expand">
-                <md-list-item class="md-inset">World</md-list-item>
-                <md-list-item class="md-inset">Europe</md-list-item>
-                <md-list-item class="md-inset">South America</md-list-item>
-            </md-list>
-            </md-list-item>
+                  <md-list-item md-expand>
+                    <md-icon>aspect_ratio</md-icon>
+                    <span class="md-list-item-text">Área de trabalho</span>
 
-            <md-list-item md-expand>
-            <md-icon>videogame_asset</md-icon>
-            <span class="md-list-item-text">Games</span>
+                    <md-list slot="md-expand">
+                      <md-list-item class="md-inset">Trabalhos</md-list-item>
+                      <md-list-item class="md-inset">Atividades</md-list-item>
+                    </md-list>
+                  </md-list-item>
 
-            <md-list slot="md-expand">
-                <md-list-item class="md-inset">Console</md-list-item>
-                <md-list-item class="md-inset">PC</md-list-item>
-                <md-list-item class="md-inset">Phone</md-list-item>
-            </md-list>
-            </md-list-item>
+                  <span class="md-list-item-text">Ferramentas</span>
 
-            <md-list-item md-expand>
-            <md-icon>video_library</md-icon>
-            <span class="md-list-item-text">Video</span>
+                  <md-list-item>
+                    <md-icon>mail_outline</md-icon>
+                    <span class="md-list-item-text">Email</span>
+                  </md-list-item>
 
-            <md-list slot="md-expand">
-                <md-list-item class="md-inset">Humor</md-list-item>
-                <md-list-item class="md-inset">Music</md-list-item>
-                <md-list-item class="md-inset">Movies</md-list-item>
-                <md-list-item class="md-inset">TV Shows</md-list-item>
-            </md-list>
-            </md-list-item>
+                  <md-list-item>
+                    <md-icon>chat_bubble_outline</md-icon>
+                    <span class="md-list-item-text">Chat</span>
+                  </md-list-item>
 
-            <md-list-item>
-            <md-icon>shopping_basket</md-icon>
-            <span class="md-list-item-text">Shop</span>
-            </md-list-item>
-        </md-list>
-        </div>
-    </div>
+                  <md-list-item>
+                    <md-icon>done_outline</md-icon>
+                    <span class="md-list-item-text">To Do</span>
+                  </md-list-item>
+              </md-list>
+            </div>
+          </div>
       </md-app-drawer>
 
       <md-app-content>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error quibusdam, non molestias et! Earum magnam, similique, quo recusandae placeat dicta asperiores modi sint ea repudiandae maxime? Quae non explicabo, neque.
+        <aside class="md-layout">
+           <md-card class="md-layout-item md-size-25 md-small-size-100 cards" md-with-hover>
+            <md-ripple>
+              <md-card-header>
+                <div class="md-title">Programação III</div>
+              </md-card-header>
+
+              <md-card-media>
+                <img src="../assets/progress.png" alt="Progresso da disciplina" height="80px" width="80px">
+              </md-card-media>
+
+              <md-card-content>
+                Porcentagem de conclusão da disciplina, a cada atividade concluída e enviada ao professor, a porcentagem será atualizada, assim como qualquer ação referente as atividades pendentes da disciplina.
+              </md-card-content>
+            </md-ripple>
+          </md-card>
+
+          <md-card class="md-layout-item md-size-50 md-small-size-100 cards" md-with-hover>
+            <md-ripple>
+              <md-card-header>
+                <div class="md-title">Programação III</div>
+              </md-card-header>
+
+              <md-card-content>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio itaque ea, nostrum odio. Dolores, sed accusantium quasi non.
+              </md-card-content>
+            </md-ripple>
+          </md-card>
+
+          <md-card class="md-layout-item md-size-25 md-small-size-100 cards" md-with-hover>
+            <md-ripple>
+              <md-card-header>
+                <div class="md-title">Programação III</div>
+              </md-card-header>
+
+              <md-card-content>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio itaque ea, nostrum odio. Dolores, sed accusantium quasi non.
+              </md-card-content>
+            </md-ripple>
+          </md-card>
+
+          <md-card class="md-layout-item md-size-50 md-small-size-100 cards" md-with-hover>
+            <md-ripple>
+              <md-card-header>
+                <div class="md-title">Programação III</div>
+              </md-card-header>
+
+              <md-card-content>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio itaque ea, nostrum odio. Dolores, sed accusantium quasi non.
+              </md-card-content>
+            </md-ripple>
+          </md-card>
+        </aside>
       </md-app-content>
     </md-app>
   </div>
@@ -66,7 +111,7 @@
 <style lang="scss" scoped>
 
   .md-drawer {
-    width: 230px;
+    width: 320px;
     max-width: calc(100vw - 125px);
   }
 
@@ -86,7 +131,7 @@
     max-width: 100%;
     display: inline-block;
     vertical-align: top;
-    border: 1px solid rgba(#000, .12);
+    box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
   }
 
   $list-width: 320px;
@@ -104,10 +149,8 @@
   .full-control > .md-list {
     width: $list-width;
     max-width: 100%;
-    height: 400px;
     display: inline-block;
     overflow: auto;
-    border: 1px solid rgba(#000, .12);
     vertical-align: top;
   }
 
@@ -116,6 +159,10 @@
     display: flex;
     flex-direction: column;
     padding: 16px;
+  }
+
+  .cards{
+    margin-top: 50px;
   }
 </style>
 
