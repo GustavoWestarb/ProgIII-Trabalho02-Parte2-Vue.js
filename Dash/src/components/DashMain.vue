54e71a7f0a220d7645d05fdc7d60d9f18c9dc0c7
Gustavo Westarb <gustavowestarb45@gmail.com>
2019-04-21 21:11:24 -0300

    Implementações do DashBoard em vue

--- Dash/src/components/DashMain.vue
+++ Dash/src/components/DashMain.vue
@@ -1,18 +1,37 @@
 <template>
-  <div class="page-container md-layout-row">
-      <md-app>
-        <md-app-toolbar class="md-primary header">
-          <span class="md-title">Dash</span>
+  <div class="page-container">
+      <md-app md-waterfall md-mode="overlap">
+        <md-app-toolbar class="md-primary md-large header">
+          <span class="md-title">Sistema Dash</span>
+
+          <div class="md-toolbar-section-end">
+            <md-field class="md-layout-item md-size-25">
+              <label style="color: white">Search</label>
+              <span class="md-prefix"><md-icon>search</md-icon></span>
+              <md-input></md-input>
+            </md-field>
+
+            <md-button class="md-icon-button">
+              <md-icon>notifications_active</md-icon>
+            </md-button>
+
+            <md-button class="md-icon-button">
+              <md-icon>more_vert</md-icon>
+            </md-button>
+
+            <md-button class="md-icon-button">
+              <md-icon>account_circle</md-icon>
+            </md-button>
+          </div>
         </md-app-toolbar>
 
         <md-app-drawer md-permanent="full">
           <md-toolbar class="md-transparent logo-menu" md-elevation="0">
-            LOGO
+            <img src="../assets/dash.png" alt="Logo dash" height="50px" width="200px"/>
           </md-toolbar>
-
-          <div class="sidebar-wrapper">
+          <div>
             <div class="list">
-              <md-list :md-expand-single="expandSingle">
+              <md-list>
                   <md-list-item>
                     <md-icon>card_travel</md-icon>
                     <span class="md-list-item-text">Disciplinas</span>
@@ -44,71 +63,110 @@
                     <md-icon>done_outline</md-icon>
                     <span class="md-list-item-text">To Do</span>
                   </md-list-item>
-              </md-list>
+
+                </md-list>
+              </div>
             </div>
+          </md-app-drawer>
+
+        <md-app-content>
+          <div class="md-layout">
+            <md-card class="md-layout-item md-size-25 md-small-size-100 card" md-with-hover>
+              <md-ripple>
+               <md-card-header>
+                 <div class="md-title">Programação III</div>
+                </md-card-header>
+
+                <md-card-media md-ratio>
+                  <img src="../assets/progress.png" alt="Progresso da disciplina">
+                </md-card-media>
+
+                <md-card-content>
+                  Porcentagem de conclusão da disciplina, a cada atividade concluída e enviada ao professor, a porcentagem será atualizada.
+                </md-card-content>
+              </md-ripple>
+            </md-card>
+
+            <md-card class="md-layout-item md-size-70 md-small-size-100 card" md-with-hover>
+              <md-ripple>
+                <md-card-header>
+                  <div class="md-title">Programação III</div>
+                </md-card-header>
+                <md-card-media md-ratio>
+                  <img src="../assets/card02.png" alt="Progresso da disciplina">
+                </md-card-media>
+              </md-ripple>
+            </md-card>
+
+            <form novalidate @submit.prevent="validateUser">
+              <md-card class="md-layout-item md-size-50 md-small-size-100 card" md-with-hover>
+                <md-ripple>
+                  <md-card-header>
+                    <div class="md-title">Dados</div>
+                  </md-card-header>
+
+                  <md-card-content>
+                    <div class="md-layout md-gutter">
+                      <div class="md-layout-item md-small-size-100">
+                        <md-field>
+                          <label for="first-name">First Name</label>
+                          <md-input name="first-name" id="first-name" autocomplete="given-name" />
+                        </md-field>
+                      </div>
+
+                      <div class="md-layout-item md-small-size-100">
+                        <md-field>
+                          <label for="first-name">First Name</label>
+                          <md-input name="first-name" id="first-name" autocomplete="given-name" />
+                        </md-field>
+                      </div>
+
+                      <div class="md-layout-item md-small-size-100">
+                        <md-field>
+                          <label for="first-name">First Name</label>
+                          <md-input name="first-name" id="first-name" autocomplete="given-name" />
+                        </md-field>
+                      </div>
+
+                      <div class="md-layout-item md-small-size-100">
+                        <md-field>
+                          <label for="first-name">First Name</label>
+                          <md-input name="first-name" id="first-name" autocomplete="given-name" />
+                        </md-field>
+                      </div>
+                    </div>
+                  </md-card-content>
+                </md-ripple>
+              </md-card>
+            </form>
+
+            <md-card class="md-layout-item md-size-25 md-small-size-100 card" md-with-hover>
+              <md-ripple>
+                <md-card-header>
+                  <div class="md-title">Programação III</div>
+                </md-card-header>
+
+                <md-card-content>
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio itaque ea, nostrum odio. Dolores, sed accusantium quasi non.
+                </md-card-content>
+              </md-ripple>
+            </md-card>
           </div>
-      </md-app-drawer>
-
-      <md-app-content>
-        <aside class="md-layout">
-           <md-card class="md-layout-item md-size-25 md-small-size-100 cards" md-with-hover>
-            <md-ripple>
-              <md-card-header>
-                <div class="md-title">Programação III</div>
-              </md-card-header>
-
-              <md-card-media>
-                <img src="../assets/progress.png" alt="Progresso da disciplina" height="80px" width="80px">
-              </md-card-media>
-
-              <md-card-content>
-                Porcentagem de conclusão da disciplina, a cada atividade concluída e enviada ao professor, a porcentagem será atualizada, assim como qualquer ação referente as atividades pendentes da disciplina.
-              </md-card-content>
-            </md-ripple>
-          </md-card>
-
-          <md-card class="md-layout-item md-size-50 md-small-size-100 cards" md-with-hover>
-            <md-ripple>
-              <md-card-header>
-                <div class="md-title">Programação III</div>
-              </md-card-header>
-
-              <md-card-content>
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio itaque ea, nostrum odio. Dolores, sed accusantium quasi non.
-              </md-card-content>
-            </md-ripple>
-          </md-card>
-
-          <md-card class="md-layout-item md-size-25 md-small-size-100 cards" md-with-hover>
-            <md-ripple>
-              <md-card-header>
-                <div class="md-title">Programação III</div>
-              </md-card-header>
-
-              <md-card-content>
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio itaque ea, nostrum odio. Dolores, sed accusantium quasi non.
-              </md-card-content>
-            </md-ripple>
-          </md-card>
-
-          <md-card class="md-layout-item md-size-50 md-small-size-100 cards" md-with-hover>
-            <md-ripple>
-              <md-card-header>
-                <div class="md-title">Programação III</div>
-              </md-card-header>
-
-              <md-card-content>
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio itaque ea, nostrum odio. Dolores, sed accusantium quasi non.
-              </md-card-content>
-            </md-ripple>
-          </md-card>
-        </aside>
-      </md-app-content>
-    </md-app>
-  </div>
+        </md-app-content>
+      </md-app>
+    </div>
 </template>
 
 <style lang="scss" scoped>
+
+  .md-app {
+    height: 100%;
+    border: 1px solid rgba(#000, .12);
+  }
+
+  .md-app-drawer{
+    box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+  }
 
   .md-drawer {
     width: 320px;
@@ -130,8 +188,6 @@
     width: 320px;
     max-width: 100%;
     display: inline-block;
-    vertical-align: top;
-    box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
   }
 
   $list-width: 320px;
@@ -140,6 +196,7 @@
     display: flex;
     flex-direction: row;
     flex-wrap: wrap-reverse;
+    height: $list-width;
   }
 
   .list {
@@ -161,14 +218,14 @@
     padding: 16px;
   }
 
-  .cards{
+  .card{
     margin-top: 50px;
   }
 </style>
 
 <script>
 export default {
-  name: 'Normal'
+  name: 'Waterfall'
 }
 
 </script>
--- Dash/src/main.js
+++ Dash/src/main.js
@@ -4,12 +4,10 @@ import Vue from 'vue'
 import VueMaterial from 'vue-material'
 import 'vue-material/dist/vue-material.min.css'
 import 'vue-material/dist/theme/default.css'
-import App from './AppLogin'
-import Checkbox from 'vue-material-checkbox'
+import App from './App'
 import VueCookies from 'vue-cookies'
 
 Vue.use(VueMaterial)
-Vue.use(Checkbox)
 Vue.use(VueCookies)
 
 Vue.config.productionTip = false
